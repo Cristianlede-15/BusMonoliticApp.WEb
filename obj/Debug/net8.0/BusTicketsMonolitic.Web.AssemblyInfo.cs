bec29b3e9b42d965f4d20c79da41b0586c5c066a
Syo <diazdaybelsamuel@gmail.com>
2024-06-03 15:36:56 -0400

    actualizacion

--- obj/Debug/net8.0/BusTicketsMonolitic.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/BusTicketsMonolitic.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusTicketsMonolitic.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0cb5e83a1367fa3604d6e728032719a2d2db143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bdb5a19cadb944cfd8da592a2724041b1e700e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusTicketsMonolitic.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusTicketsMonolitic.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
